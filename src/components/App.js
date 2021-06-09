abae4cf2540ab29717daedb411eee67e270a886b
MITHUN <mithunchide03@gmail.com>
2021-06-09 16:43:33 +0530

    adding succesfully

--- src/components/App.js
+++ src/components/App.js
@@ -4,14 +4,48 @@ import '../styles/App.css';
 const App = () => {
   // write your code here 
 
+
+const [inputTime,setInputTime] = useState(0);
+const[timerId,setTimerId]= useState(0);
+
+const countDownTimer = (e)=>{
+  if(e.which !== 13)
+  return ;
+  clearInterval(timerId);
+  setInputTime(0);
+  let input = document.getElementById("timerCount").value;
+  input = Number(input);
+  input = Math.floor(input)
+  if(input === "0" || input === "" || input <=0 || isNaN(input))
+  return;
+  setInputTime(input);
+  setTimerId(setInterval(()=>{
+    setInputTime((prev) => prev-1);
+  },1000));
+
+};
+useEffect (()=>{
+  if(inputTime<0){
+    clearInterval(timerId);
+    setInputTime(0);
+  }
+},[inputTime]);
+
+useEffect(()=>{
+  return ()=>{
+    clearInterval(timerId);
+  }
+},[]);
+
+
   return (
     <div className="wrapper">
       <div id="whole-center">
         <h1>
-          Reverse countdown for<input id="timeCount" onKeyDown={/* callback function */} /> sec.
+          Reverse countdown for<input id="timeCount" onKeyDown={(e)=>{countDownTimer(e)}} />{""} sec.
         </h1>
       </div>
-      <div id="current-time">{/* remaining time */}</div>
+      <div id="current-time">{/* remaining time */ inputTime}</div>
     </div>
   )
 }
