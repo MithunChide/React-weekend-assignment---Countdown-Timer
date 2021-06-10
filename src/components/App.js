c61c2a31ad7ccf9d541d99b0dadafca16b1a94cd
MITHUN <mithunchide03@gmail.com>
2021-06-10 13:23:42 +0530

    run succesfully

--- src/components/App.js
+++ src/components/App.js
@@ -1,54 +1,58 @@
 import React, { Component, useState, useEffect } from "react";
-import '../styles/App.css';
+import "../styles/App.css";
 
 const App = () => {
-  // write your code here 
+    // write your code here
 
+    const [inputTime, setInputTime] = useState(0);
+    const [timerID, settimerID] = useState(0);
+    const countDownTimer = (e) => {
+        if (e.which !== 13) return;
+        clearInterval(timerID);
+        setInputTime(0);
+        let input = document.getElementById("timeCount").value;
+        input = Number(input);
+        input = Math.floor(input);
+        if (input === "0" || input === "" || input <= 0 || isNaN(input)) return;
 
-const [inputTime,setInputTime] = useState(0);
-const[timerId,setTimerId]= useState(0);
+        setInputTime(input);
+        settimerID(
+            setInterval(() => {
+                setInputTime((prev) => prev - 1);
+            }, 1000)
+        );
+    };
 
-const countDownTimer = (e)=>{
-  if(e.which !== 13)
-  return ;
-  clearInterval(timerId);
-  setInputTime(0);
-  let input = document.getElementById("timerCount").value;
-  input = Number(input);
-  input = Math.floor(input)
-  if(input === "0" || input === "" || input <=0 || isNaN(input))
-  return;
-  setInputTime(input);
-  setTimerId(setInterval(()=>{
-    setInputTime((prev) => prev-1);
-  },1000));
+    useEffect(() => {
+        if (inputTime < 0) {
+            clearInterval(timerID);
+            setInputTime(0);
+        }
+    }, [inputTime]);
 
+    useEffect(() => {
+        return () => {
+            clearInterval(timerID);
+        };
+    }, []);
+
+    return (
+        <div className="wrapper">
+            <div id="whole-center">
+                <h1>
+                    Reverse countdown for
+                    <input
+                        id="timeCount"
+                        onKeyDown={(e) => {
+                            countDownTimer(e);
+                        }}
+                    />{" "}
+                    sec.
+                </h1>
+            </div>
+            <div id="current-time">{/* remaining time */ inputTime}</div>
+        </div>
+    );
 };
-useEffect (()=>{
-  if(inputTime<0){
-    clearInterval(timerId);
-    setInputTime(0);
-  }
-},[inputTime]);
-
-useEffect(()=>{
-  return ()=>{
-    clearInterval(timerId);
-  }
-},[]);
-
-
-  return (
-    <div className="wrapper">
-      <div id="whole-center">
-        <h1>
-          Reverse countdown for<input id="timeCount" onKeyDown={(e)=>{countDownTimer(e)}} />{""} sec.
-        </h1>
-      </div>
-      <div id="current-time">{/* remaining time */ inputTime}</div>
-    </div>
-  )
-}
-
 
 export default App;
